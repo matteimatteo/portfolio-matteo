0df2b065bdb41ee09617d9fa41f4d0fdc44fc62e
Mattéo <matteo_turpin-hannequand@ens.univ-artois.fr>
2026-06-11 10:54:25 +0200

    Update login-form.tsx

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,5 +1,4 @@
-import { lusitana } from '@/app/ui/fonts';
-import {
+import { inter } from '@/app/ui/fonts';import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
@@ -11,7 +10,7 @@ export default function LoginForm() {
   return (
     <form className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+        <h1 className={${inter.className} mb-3 text-2xl`}>
           Please log in to continue.
         </h1>
         <div className="w-full">
